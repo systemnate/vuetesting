1012fd5a38619eaf4b3df1161815a1bbd66a5e4a
Nate Dalo <ndalo@merchantsbonding.com>
2017-07-18 09:29:03 -0500

    initial commit

---
+++ test/javascript/App.spec.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import App from '../../app/javascript/packs/App.vue';
+const chai = require('chai')
+const expect = chai.expect
+
+describe('first test', () => {
+  it('is true', () => {
+    expect(true).to.equal(true)
+  })
+})
